1538361e223ae9a93a26bb6aefd7029bc4133c5a
Darius Laurinčikas <darius.laurincikas@adform.com>
2015-07-09 20:18:26 +0300

    Step 1 Start project

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+    runSeq = require('run-sequence'),
+    bower = require('gulp-bower');
+
+//server module
+var opn = require('opn'),
+    connect = require('connect'),
+    serveStatic = require('serve-static'),
+    serveIndex = require('serve-index'),
+    http = require('http'),
+    history = require('connect-history-api-fallback');
+
+var config = {
+    buildDir: './src/',
+    port: '8888'
+};
+
+gulp.task('bower',function() {
+    bower();
+});
+
+gulp.task( 'connect', function() {
+    var app = connect()
+        .use(history())
+        .use(serveStatic(config.buildDir))
+        .use( '/bower_components', serveStatic( 'bower_components' ) )
+        .use(serveIndex( config.buildDir ));
+
+    http.createServer( app )
+        .listen( config.port )
+        .on( 'listening', function() {
+            opn( 'http://localhost:'+ config.port);
+            console.log( 'Started connect web server on http://localhost:' + config.port );
+        } );
+} );
+
+gulp.task('default', function() {
+    return runSeq('connect');
+});
